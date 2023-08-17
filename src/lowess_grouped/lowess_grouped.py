2defa7c38c535b73d6ab82b5bc5f73e2dbbb816a
Lukas Wieser <lukas.luki.wieser@gmail.com>
2023-08-17 14:04:08 +0200

    Add docstrings and datatypes to parameters of function lowess_grouped

--- src/lowess_grouped/lowess_grouped.py
+++ src/lowess_grouped/lowess_grouped.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Literal
 
 import pandas as pd
 import statsmodels.api as sm
@@ -9,18 +9,32 @@ def lowess_grouped(
         x_name: str,
         y_name: str,
         group_name: Union[str, None],
-        smoothed_col_suffix="_smooth",
-        frac=0.6666666666666666,
-        it=3,
-        delta=0.0,
-        is_sorted=False,
-        missing='drop',
-        return_sorted=True
+        smoothed_col_suffix: str = "_smooth",
+        frac: float = 0.6666666666666666,
+        it: int = 3,
+        delta: float = 0.0,
+        is_sorted: bool = False,
+        missing: Literal['none', 'drop', 'raise'] = 'drop',
+        return_sorted: bool = True
 ) -> pd.DataFrame:
     """
-    applies lowess smoothing to each group
-    if no group is supplied lowess will be applied to the whole dataset
+    Applies lowess smoothing to each group.
+    If no group is supplied, lowess will be applied to the whole dataset.
+
+    :param data: input dataframe containing the data to be smoothed
+    :param x_name: name of the column representing the independent variable (x-axis)
+    :param y_name: name of the column representing the dependent variable (y-axis)
+    :param group_name: name of the column indicating the group for the groupwise smoothing. If None, smoothing will be applied to the entire dataset
+    :param smoothed_col_suffix: suffix to be added to the column name of the smoothed y-values
+    :param frac: how strongly to smooth the data. Between 0 and 1. Specifically the fraction of the data used when smoothing each y-value
+    :param it: number of residual-based reweightings to perform
+    :param delta: distance within which to use linear-interpolation instead of weighted regression.
+    :param is_sorted: If False, then the data will be sorted by x-values before calculating lowess. If True, then it is assumed that the data is already sorted by x-values.
+    :param missing: If ‘none’, no nan checking is done. If ‘drop’, any observations with nans are dropped. If ‘raise’, an error is raised.
+    :param return_sorted: If True, then the returned array is sorted by x-values and has missing (nan or infinite) observations removed. If False, then the returned array is in the same length and the same sequence of observations as the input array.
+    :return: copy of the original dataframe, with a new column added for the smoothed y-values
     """
+
     df = data.copy()
     y_name_smoothed = y_name + smoothed_col_suffix
     if group_name is not None:
