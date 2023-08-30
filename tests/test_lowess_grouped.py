8bd81c51e7d9921ee81998cde1cd0d0e83944259
Lukas Wieser <lukas.luki.wieser@gmail.com>
2023-08-30 18:36:11 +0200

    Add testcases for smoothed column suffix

--- tests/test_lowess_grouped.py
+++ tests/test_lowess_grouped.py
@@ -81,5 +81,59 @@ class TestLowessGrouped(unittest.TestCase):
             )
 
 
+class TestSmoothingSuffix(unittest.TestCase):
+    def setUp(self):
+        # load the data once for all tests
+        self.temp_region = pd.read_csv(DATA_DIR / "temperature-by-region.csv")
+
+    def test_str_suffix_str_y_name(self):
+        # copy  data so that we won't accidentally change the data of other tests
+        temp_region = self.temp_region.copy()
+
+        temp_region = lowess_grouped(temp_region, "year", "temperature_anomaly", "region_name",
+                                     smoothed_col_suffix="_smooth2", frac=0.05)
+
+        column_names = list(temp_region.columns)
+        self.assertTrue(
+            "temperature_anomaly_smooth2" in column_names,
+            "Smoothed column has either wrong name, or does not exist")
+
+    def test_tuple_suffix_str_y_name(self):
+        with self.assertRaises(ValueError) as cm:
+            lowess_grouped(self.temp_region, "year", "temperature_anomaly", "region_name",
+                           smoothed_col_suffix=("_smooth2", "_smooth3"), frac=0.05)
+
+        self.assertEqual(str(cm.exception), "If type of y_name is string then smoothed_col_suffix must also be string")
+
+    def test_str_suffix_tuple_y_name(self):
+        temp_region = self.temp_region.rename(columns={'temperature_anomaly': ('temperature_anomaly', 'median')})
+
+        temp_region = lowess_grouped(temp_region, "year", ('temperature_anomaly', 'median'), "region_name")
+
+        column_names = list(temp_region.columns)
+        self.assertTrue(
+            ('temperature_anomaly_smooth', 'median') in column_names,
+            "Smoothed column has either wrong name, or does not exist")
+
+    def test_tuple_suffix_tuple_y_name(self):
+        temp_region = self.temp_region.rename(columns={'temperature_anomaly': ('temperature_anomaly', 'median')})
+
+        temp_region = lowess_grouped(temp_region, "year", ('temperature_anomaly', 'median'),
+                                     "region_name", smoothed_col_suffix=('_smooth1', '_smooth2'))
+
+        column_names = list(temp_region.columns)
+        self.assertTrue(
+            ('temperature_anomaly_smooth1', 'median_smooth2') in column_names,
+            "Smoothed column has either wrong name, or does not exist")
+
+    def test_tuple_suffix_wrong_length(self):
+        temp_region = self.temp_region.rename(columns={'temperature_anomaly': ('temperature_anomaly', 'median')})
+
+        with self.assertRaises(ValueError) as cm:
+            lowess_grouped(temp_region, "year", ('temperature_anomaly', 'median'),"region_name",
+                           smoothed_col_suffix=('_smooth1','_smooth2','_smooth3'))
+
+        self.assertEqual(str(cm.exception), "Tuple of smoothed_col_suffix must have same length as tuple of y_name")
+
 if __name__ == '__main__':
     unittest.main()
